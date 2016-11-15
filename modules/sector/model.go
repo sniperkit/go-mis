ba2d026e3cff31f71e080347b358d79883e4eb9c
Ais <ais@Aiss-MacBook-Pro.local>
2016-11-15 14:52:15 +0700

    Added new module: sector

---
+++ modules/sector/model.go
@@ -0,0 +1,11 @@
+package sector
+
+import "time"
+
+type Sector struct {
+	ID        uint       `gorm:"primary_key" gorm:"column:_id" json:"_id"`
+	Name      string     `gorm:"column:name" json:"name"`
+	CreatedAt time.Time  `gorm:"column:createdAt" json:"createdAt"`
+	UpdatedAt time.Time  `gorm:"column:updatedAt" json:"updatedAt"`
+	DeletedAt *time.Time `gorm:"column:deletedAt" json:"deletedAt"`
+}
