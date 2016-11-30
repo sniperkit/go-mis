3efbe5112c14596b9702415d40cdbd4f2f189759
madebyais <madebyais@gmail.com>
2016-11-30 15:43:52 +0700

    Removed marital status. Added new field isInstutional.

--- modules/investor/model.go
+++ modules/investor/model.go
@@ -4,6 +4,7 @@ import "time"
 
 type Investor struct {
 	ID               uint       `gorm:"primary_key" gorm:"column:_id" json:"_id"`
+	IsInstitutional  bool       `gorm:"column:isInstitutional" json:"isInstitutional"`
 	IsCheckedTerm    bool       `gorm:"column:isCheckedTerm" json:"isCheckedTerm"`
 	IsCheckedPrivacy bool       `gorm:"column:isCheckedPrivacy" json:"isCheckedPrivacy"`
 	PhoneNo          uint       `gorm:"column:phoneNo" json:"phoneNo"`
@@ -12,7 +13,6 @@ type Investor struct {
 	BankBranch       string     `gorm:"column:bankbranch" json:"bankbranch"`
 	BankAccountName  string     `gorm:"column:bankAccountName" json:"bankAccountName"`
 	BankAccountNo    uint       `gorm:"column:bankAccountNo" json:"bankAccountNo"`
-	MaritalStatus    string     `gorm:"column:maritalStatus" json:"maritalStatus"`
 	CreatedAt        time.Time  `gorm:"column:createdAt" json:"createdAt"`
 	UpdatedAt        time.Time  `gorm:"column:updatedAt" json:"updatedAt"`
 	DeletedAt        *time.Time `gorm:"column:deletedAt" json:"deletedAt"`
