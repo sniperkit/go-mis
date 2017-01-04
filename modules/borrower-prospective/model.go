b196112f5d30ad5626666790e4d4b3626acbc8fb
Ais <ais@ais.local>
2017-01-04 13:31:58 +0700

    Updated borrower prospective model

--- modules/borrower-prospective/model.go
+++ modules/borrower-prospective/model.go
@@ -5,7 +5,7 @@ import "time"
 type BorrowerProspective struct {
 	ID        uint64     `gorm:"primary_key" gorm:"column:_id" json:"_id"`
 	Raw       string     `gorm:"column:raw" json:"raw" sql:"json"`
-	IsApprove bool       `gorm:"column:isStatus" json:"isStatus"`
+	IsApprove bool       `gorm:"column:isApprove" json:"isApprove"`
 	CreatedAt time.Time  `gorm:"column:createdAt" json:"createdAt"`
 	UpdatedAt time.Time  `gorm:"column:updatedAt" json:"updatedAt"`
 	DeletedAt *time.Time `gorm:"column:deletedAt" json:"deletedAt"`
