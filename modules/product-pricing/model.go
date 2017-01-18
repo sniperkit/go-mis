7fc26ac445d23addfc75fb1c318ad070cd04bc04
Ais <madebyais@gmail.com>
2017-01-18 14:47:06 +0700

    Added new fields.

--- modules/product-pricing/model.go
+++ modules/product-pricing/model.go
@@ -7,6 +7,9 @@ type ProductPricing struct {
 	ReturnOfInvestment float64    `gorm:"column:returnOfInvestment" json:"returnOfInvestment"`
 	AdminitrationFee   float64    `gorm:"column:administrationFee" json:"administrationFee"`
 	ServiceFee         float64    `gorm:"column:serviceFee" json:"serviceFee"`
+	StartDate          *time.Time `gorm:"column:startDate" json:"startDate"`
+	EndDate            *time.Time `gorm:"column:endDate" json:"endDate"`
+	IsInstutitional    bool       `gorm:"column:isInstitutional" json:"isInstitutional"`
 	CreatedAt          time.Time  `gorm:"column:createdAt" json:"createdAt"`
 	UpdatedAt          time.Time  `gorm:"column:updatedAt" json:"updatedAt"`
 	DeletedAt          *time.Time `gorm:"column:deletedAt" json:"deletedAt"`
