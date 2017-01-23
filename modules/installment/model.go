d59119d38ddd14389886c83596457917094245c7
Ais <madebyais@gmail.com>
2017-01-23 12:52:51 +0700

    Updated installment table

--- modules/installment/model.go
+++ modules/installment/model.go
@@ -3,15 +3,14 @@ package installment
 import "time"
 
 type Installment struct {
-	ID             uint64     `gorm:"primary_key" gorm:"column:_id" json:"_id"`
-	Type           string     `gorm:"column:type" json:"type"`         // NORMAL, DOUBLE, PELUNASAN DINI
-	Presence       string     `gorm:"column:presence" json:"presence"` // ABSENT, TR1, TR2, TR3
-	Principal      float64    `gorm:"column:principal" json:"principal"`
-	ProfitAmf      float64    `gorm:"column:profitAmf" json:"profitAmf"`
-	ProfitInvestor float64    `gorm:"column:profitInvestor" json:"profitInvestor"`
-	Reserve        float64    `gorm:"column:reserve" json:"reserve"`
-	Stage          string     `gorm:"column:stage" json:"stage"`
-	CreatedAt      time.Time  `gorm:"column:createdAt" json:"createdAt"`
-	UpdatedAt      time.Time  `gorm:"column:updatedAt" json:"updatedAt"`
-	DeletedAt      *time.Time `gorm:"column:deletedAt" json:"deletedAt"`
+	ID              uint64     `gorm:"primary_key" gorm:"column:_id" json:"_id"`
+	Type            string     `gorm:"column:type" json:"type"`         // NORMAL, DOUBLE, PELUNASAN DINI
+	Presence        string     `gorm:"column:presence" json:"presence"` // ATTEND, ABSENT, TR1, TR2, TR3
+	PaidInstallment float64    `gorm:"column:paidInstallment" json:"paidInstallment"`
+	Penalty         float64    `gorm:"column:penalty" json:"penalty"`
+	Reserve         float64    `gorm:"column:reserve" json:"reserve"`
+	Stage           string     `gorm:"column:stage" json:"stage"`
+	CreatedAt       time.Time  `gorm:"column:createdAt" json:"createdAt"`
+	UpdatedAt       time.Time  `gorm:"column:updatedAt" json:"updatedAt"`
+	DeletedAt       *time.Time `gorm:"column:deletedAt" json:"deletedAt"`
 }
