95a97e82c22a20cf74a38d83b39da8e6e7424a59
Septiyan Andika <septiyan.andika@gmail.com>
2017-01-26 12:30:10 +0700

    Aera & cif

--- modules/area/controller.go
+++ modules/area/controller.go
@@ -1,8 +1,25 @@
 package area
 
-import "bitbucket.org/go-mis/services"
+import (
+	"bitbucket.org/go-mis/services"
+	iris "gopkg.in/kataras/iris.v4"
+)
 
 func Init() {
 	services.DBCPsql.AutoMigrate(&Area{})
 	services.BaseCrudInit(Area{}, []Area{})
+}
+
+// FetchAll - fetchAll agent data
+func FetchAll(ctx *iris.Context) {
+	areas := []Area{}
+	services.DBCPsql.Where("\"deletedAt\" IS NULL").Order("id asc").Find(&areas)
+	ctx.JSON(iris.StatusOK, iris.Map{"data": areas})
+}
+
+// GetByID agent by id
+func GetByID(ctx *iris.Context) {
+	area := Area{}
+	services.DBCPsql.Where("\"deletedAt\" IS NULL AND id = ?", ctx.Param("id")).First(&area)
+	ctx.JSON(iris.StatusOK, iris.Map{"data": area})
 }
--- modules/cif/controller.go
+++ modules/cif/controller.go
@@ -1,8 +1,25 @@
 package cif
 
-import "bitbucket.org/go-mis/services"
+import (
+	"bitbucket.org/go-mis/services"
+	iris "gopkg.in/kataras/iris.v4"
+)
 
 func Init() {
 	services.DBCPsql.AutoMigrate(&Cif{})
 	services.BaseCrudInit(Cif{}, []Cif{})
+}
+
+// FetchAll - fetchAll agent data
+func FetchAll(ctx *iris.Context) {
+	cifs := []Cif{}
+	services.DBCPsql.Where("\"deletedAt\" IS NULL").Order("id asc").Find(&cifs)
+	ctx.JSON(iris.StatusOK, iris.Map{"data": cifs})
+}
+
+// GetByID agent by id
+func GetByID(ctx *iris.Context) {
+	cif := Cif{}
+	services.DBCPsql.Where("\"deletedAt\" IS NULL AND id = ?", ctx.Param("id")).First(&cif)
+	ctx.JSON(iris.StatusOK, iris.Map{"data": cif})
 }
