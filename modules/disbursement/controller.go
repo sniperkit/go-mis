d45850db613e4f4cc76f72907d54851aa3042431
Ais <madebyais@gmail.com>
2017-02-01 12:37:04 +0700

    Updated disbursement. Updated installment.

--- modules/disbursement/controller.go
+++ modules/disbursement/controller.go
@@ -14,15 +14,21 @@ func Init() {
 func FetchAll(ctx *iris.Context) {
 	disbursements := []DisbursementFetch{}
 
-	query := "SELECT disbursement.\"id\", disbursement.\"disbursementDate\", disbursement.\"stage\", disbursement.\"createdAt\" "
-	query += ", \"group\".\"name\" as \"groupName\", branch.\"name\" as \"branchName\"  "
+	query := "SELECT disbursement.\"id\", disbursement.\"disbursementDate\", disbursement.\"stage\", "
+	query += "loan.\"submittedLoanDate\", loan.\"plafond\", \"group\".\"name\" as \"group\", branch.\"name\" as \"branch\", "
+	query += "cif.\"name\" as \"borrower\" "
 	query += "FROM disbursement "
 	query += "JOIN r_loan_disbursement ON r_loan_disbursement.\"disbursementId\" = disbursement.\"id\" "
 	query += "JOIN loan ON r_loan_disbursement.\"loanId\" = loan.\"id\" "
+	query += "JOIN r_loan_borrower ON r_loan_borrower.\"loanId\" = loan.\"id\" "
+	query += "JOIN borrower ON borrower.\"id\" = r_loan_borrower.\"borrowerId\" "
+	query += "JOIN r_cif_borrower ON r_cif_borrower.\"borrowerId\" = borrower.\"id\" "
+	query += "JOIN cif ON cif.\"id\" = r_cif_borrower.\"cifId\" "
 	query += "LEFT JOIN r_loan_branch ON r_loan_branch.\"loanId\" = loan.\"id\" "
 	query += "LEFT JOIN branch ON r_loan_branch.\"branchId\" = branch.\"id\" "
 	query += "LEFT JOIN r_loan_group ON r_loan_group.\"loanId\" = loan.\"id\" "
 	query += "LEFT JOIN \"group\" ON r_loan_group.\"groupId\" = \"group\".\"id\" "
+	query += "WHERE disbursement.\"stage\" IN ('PENDING', 'FAILED') "
 
 	services.DBCPsql.Raw(query).Find(&disbursements)
 	ctx.JSON(iris.StatusOK, iris.Map{"data": disbursements})
--- modules/disbursement/model.go
+++ modules/disbursement/model.go
@@ -12,10 +12,13 @@ type Disbursement struct {
 }
 
 type DisbursementFetch struct {
-	ID               uint64    `gorm:"primary_key" gorm:"column:_id" json:"_id"`
-	DisbursementDate time.Time `gorm:"column:disbursementDate" json:"disbursementDate"`
-	Stage            string    `gorm:"column:stage" json:"stage"`
-	CreatedAt        time.Time `gorm:"column:createdAt" json:"createdAt"`
-	GroupName        string    `gorm:"column:groupName" json:"groupName"`
-	BranchName       string    `gorm:"column:branchName" json:"branchName"`
+	ID                uint64    `gorm:"primary_key" gorm:"column:_id" json:"_id"`
+	Borrower          string    `gorm:"column:borrower" json:"borrower"`
+	Group             string    `gorm:"column:group" json:"group"`
+	Branch            string    `gorm:"column:branch" json:"branch"`
+	SubmittedLoanDate time.Time `gorm:"column:submittedLoanDate" json:"submittedLoanDate"`
+	DisbursementDate  time.Time `gorm:"column:disbursementDate" json:"disbursementDate"`
+	Plafond           float64   `gorm:"column:plafond" json:"plafond"`
+	Stage             string    `gorm:"column:stage" json:"stage"`
+	CreatedAt         time.Time `gorm:"column:createdAt" json:"createdAt"`
 }
