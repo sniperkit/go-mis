892eceebf367de203f340cf3d15b18d6787f90f4
egon12 <egon.firman@gmail.com>
2017-02-03 16:31:42 +0700

    Fix branch, handle redudancy branch

--- modules/branch/controller.go
+++ modules/branch/controller.go
@@ -11,24 +11,50 @@ func Init() {
 }
 
 // FetchAll - fetchAll branchs data
+/** Todo error handling */
 func FetchAll(ctx *iris.Context) {
-	branchManagerArea := []BranchManagerArea{}
+	branch := getBranchWithoutManager()
+	branchManager := getBranchManager()
+	branch = combineBranchManager(branch, branchManager)
 
-	query := "SELECT branch.\"id\", branch.\"name\", branch.\"city\", branch.\"province\", user_mis.\"fullname\" as \"manager\", area.\"name\" as \"area\", role.\"name\" as \"role\" "
-	query += "FROM branch "
-	query += "LEFT JOIN r_branch_user_mis ON r_branch_user_mis.\"branchId\" = branch.\"id\" "
-	query += "LEFT JOIN user_mis ON user_mis.\"id\" = r_branch_user_mis.\"userMisId\" "
-	query += "LEFT JOIN r_user_mis_role ON r_user_mis_role.\"userMisId\" = user_mis.\"id\" "
-	query += "LEFT JOIN role ON role.\"id\" = r_user_mis_role.\"roleId\" "
-	query += "LEFT JOIN r_area_branch ON r_area_branch.\"branchId\" = branch.\"id\" "
-	query += "LEFT JOIN area ON r_area_branch.\"areaId\" = area.\"id\" "
-	// query += "WHERE role.\"name\" = ? or role.\"id\" IS NULL"
-
-	services.DBCPsql.Raw(query).Find(&branchManagerArea)
 	ctx.JSON(iris.StatusOK, iris.Map{
 		"status": "success",
-		"data":   branchManagerArea,
+		"data":   branch,
 	})
+}
+
+func getBranchWithoutManager() []BranchManagerArea {
+	query := "SELECT branch.\"id\", branch.\"name\", branch.\"city\", branch.\"province\", area.\"name\" as \"area\" "
+	query += "FROM branch "
+	query += "LEFT JOIN r_area_branch ON r_area_branch.\"branchId\" = branch.\"id\" "
+	query += "LEFT JOIN area ON r_area_branch.\"areaId\" = area.\"id\" "
+
+	result := []BranchManagerArea{}
+	services.DBCPsql.Raw(query).Find(&result)
+	return result
+}
+
+func getBranchManager() []BranchManager {
+	query := "SELECT \"branchId\",\"fullname\" "
+	query += "FROM r_branch_user_mis rbum "
+	query += "JOIN user_mis um on um.id = rbum.\"userMisId\" "
+	query += "LEFT JOIN r_user_mis_role rumr on rbum.\"userMisId\" = rumr.\"userMisId\" "
+	//query += "WHERE roleId = 4 "
+
+	result := []BranchManager{}
+	services.DBCPsql.Raw(query).Find(&result)
+	return result
+}
+
+func combineBranchManager(branch []BranchManagerArea, branchManager []BranchManager) []BranchManagerArea {
+	for i := 0; i < len(branch); i++ {
+		for j := 0; j < len(branchManager); j++ {
+			if branch[i].ID == branchManager[j].BranchId {
+				branch[i].Manager = branchManager[j].Fullname
+			}
+		}
+	}
+	return branch
 }
 
 // GetByID branch by id
