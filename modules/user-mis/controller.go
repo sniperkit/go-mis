ccd8f8d7c47a3e7a830e4fbe71bd5a9d5289dbed
Ais <ais@Aiss-MacBook-Pro.local>
2017-02-07 16:04:31 +0700

    Added deletedAt is null. Deploy latest build.

--- modules/user-mis/controller.go
+++ modules/user-mis/controller.go
@@ -15,12 +15,13 @@ func FetchUserMisAreaBranchRole(ctx *iris.Context) {
 
 	query := "SELECT user_mis.\"id\" AS \"userMisId\", user_mis.\"picUrl\", user_mis.\"fullname\", role.\"name\" AS \"role\", branch.\"name\" AS \"branch\", area.\"name\" AS \"area\" "
 	query += "FROM user_mis "
-	query += "JOIN r_branch_user_mis ON r_branch_user_mis.\"userMisId\" = user_mis.\"id\" "
-	query += "JOIN branch ON branch.\"id\" = r_branch_user_mis.\"branchId\" "
-	query += "JOIN r_area_branch ON r_area_branch.\"branchId\" = branch.\"id\" "
-	query += "JOIN area ON area.\"id\" = r_area_branch.\"areaId\" "
-	query += "JOIN r_user_mis_role ON r_user_mis_role.\"userMisId\" = user_mis.\"id\" "
-	query += "JOIN role ON role.\"id\" = r_user_mis_role.\"roleId\" "
+	query += "LEFT JOIN r_branch_user_mis ON r_branch_user_mis.\"userMisId\" = user_mis.\"id\" "
+	query += "LEFT JOIN branch ON branch.\"id\" = r_branch_user_mis.\"branchId\" "
+	query += "LEFT JOIN r_area_branch ON r_area_branch.\"branchId\" = branch.\"id\" "
+	query += "LEFT JOIN area ON area.\"id\" = r_area_branch.\"areaId\" "
+	query += "LEFT JOIN r_user_mis_role ON r_user_mis_role.\"userMisId\" = user_mis.\"id\" "
+	query += "LEFT JOIN role ON role.\"id\" = r_user_mis_role.\"roleId\" "
+	query += "WHERE user_mis.\"deletedAt\" IS NULL "
 
 	services.DBCPsql.Raw(query).Find(&arrUserMisAreaBranchRole)
 	ctx.JSON(iris.StatusOK, iris.Map{
