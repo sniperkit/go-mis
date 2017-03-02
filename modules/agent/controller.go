0decf6d4fdc36013a4baca4b031f4909ba020549
Ais <madebyais@gmail.com>
2017-03-02 10:15:27 +0700

    Updated raw query for agetn.

--- modules/agent/controller.go
+++ modules/agent/controller.go
@@ -17,7 +17,7 @@ func GetAllAgentByBranchID(ctx *iris.Context) {
 	query := "SELECT agent.* "
 	query += "FROM agent "
 	query += "INNER JOIN r_branch_agent ON r_branch_agent.\"agentId\" = agent.id "
-	query += "WHERE r_branch_agent.\"branchId\" = ? "
+	query += "WHERE r_branch_agent.\"branchId\" = ? AND agent.\"deletedAt\" IS NULL"
 
 	services.DBCPsql.Raw(query, branchID).Scan(&agentSchema)
 	ctx.JSON(iris.StatusOK, iris.Map{
