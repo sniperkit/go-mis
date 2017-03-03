9970fd21924eddaef2b33fbcc26996c796a81ed2
Ais <madebyais@gmail.com>
2017-03-03 18:23:49 +0700

    Fixed survey.

--- modules/survey/controller.go
+++ modules/survey/controller.go
@@ -8,7 +8,7 @@ import (
 // GetProspectiveBorrower - get prospective borrower which si not migrated
 func GetProspectiveBorrower(ctx *iris.Context) {
 	aFields := []AFields{}
-	query := "SELECT * FROM a_fields WHERE \"is_migrated\" = false"
+	query := "SELECT * FROM a_fields WHERE \"is_migrated\" = false OR is_migrated IS NULL"
 	services.DBCPsqlSurvey.Raw(query).Find(&aFields)
 
 	ctx.JSON(iris.StatusOK, iris.Map{
@@ -20,7 +20,7 @@ func GetProspectiveBorrower(ctx *iris.Context) {
 // GetProspectiveBorrowerDetail - get prospective borrower detail
 func GetProspectiveBorrowerDetail(ctx *iris.Context) {
 	aFields := []AFields{}
-	query := "SELECT * FROM a_fields WHERE \"is_migrated\" = false AND \"answer_id\" = ?"
+	query := "SELECT * FROM a_fields WHERE (\"is_migrated\" = false OR is_migrated IS NULL) AND \"answer_id\" = ?"
 	query += " ORDER BY \"key\" ASC"
 	services.DBCPsqlSurvey.Raw(query, ctx.Param("id")).Find(&aFields)
 
