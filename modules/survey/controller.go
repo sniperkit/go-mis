9cf2c2e3501975231a0e97dd265a513267a34b99
Ais <madebyais@gmail.com>
2017-03-23 08:32:30 +0700

    Added archived for prospective borrower

--- modules/survey/controller.go
+++ modules/survey/controller.go
@@ -5,7 +5,7 @@ import (
 	"gopkg.in/kataras/iris.v4"
 )
 
-// GetProspectiveBorrower - get prospective borrower which si not migrated
+// GetProspectiveBorrower - get prospective borrower which is not migrated
 func GetProspectiveBorrower(ctx *iris.Context) {
 	aFields := []AFields{}
 	query := "SELECT * FROM a_fields WHERE \"is_migrated\" = false OR is_migrated IS NULL"
@@ -17,10 +17,23 @@ func GetProspectiveBorrower(ctx *iris.Context) {
 	})
 }
 
+// GetProspectiveBorrowerArchived - get rejected prospective borrower
+func GetProspectiveBorrowerArchived(ctx *iris.Context) {
+	aFields := []AFields{}
+	query := "SELECT * FROM a_fields WHERE is_migrated = true AND is_approve = false"
+	services.DBCPsqlSurvey.Raw(query).Find(&aFields)
+
+	ctx.JSON(iris.StatusOK, iris.Map{
+		"status": "success",
+		"data":   aFields,
+	})
+}
+
 // GetProspectiveBorrowerDetail - get prospective borrower detail
 func GetProspectiveBorrowerDetail(ctx *iris.Context) {
 	aFields := []AFields{}
-	query := "SELECT * FROM a_fields WHERE (\"is_migrated\" = false OR is_migrated IS NULL) AND \"answer_id\" = ?"
+	// query := "SELECT * FROM a_fields WHERE (\"is_migrated\" = false OR is_migrated IS NULL) AND \"answer_id\" = ?"
+	query := "SELECT * FROM a_fields WHERE \"answer_id\" = ?"
 	query += " ORDER BY \"key\" ASC"
 	services.DBCPsqlSurvey.Raw(query, ctx.Param("id")).Find(&aFields)
 
