d7c72d3ccc196575971cce540c458a3f381f957f
alfin akhret <alfin.akhret@amartha.com>
2017-03-31 16:03:05 +0700

    update voucher model

--- modules/voucher/model.go
+++ modules/voucher/model.go
@@ -7,12 +7,12 @@ import (
 type Voucher struct {
 	ID        				uint64    `gorm:"primary_key" gorm:"column:_id" json:"_id"`
 	Amount    				uint64    `gorm:"column:amount" json:"amount"`
-	VoucherNo 				string    `gorm:"column:voucherrNo" json:"voucherNo"`
+	VoucherNo 				string    `gorm:"column:voucherNo" json:"voucherNo"`
 	Description 			string    `gorm:"column:description" json:"description"`
-	StarDate 					time.Time `gorm:"column:startDate" json:"startDate"`
-	EndDate 					time.Time `gorm:"column:endDate" json:"endDate"`
+	StarDate 					*time.Time `gorm:"column:startDate" json:"startDate"`
+	EndDate 					*time.Time `gorm:"column:endDate" json:"endDate"`
 	CreatedAt 				time.Time `gorm:"column:createdAt" json:"createdAt"`
-	UpdatedAt 					time.Time `gorm:"column:updatedAt" json:"updatedAt"`
-	DeletedAt 					time.Time `gorm:"column:deletedAt" json:"deletedAt"`
+	UpdatedAt 				time.Time `gorm:"column:updatedAt" json:"updatedAt"`
+	DeletedAt 				*time.Time `gorm:"column:deletedAt" json:"deletedAt"`
 	IsPersonal				*bool 		`gorm:"column:isPersonal" json:"isPersonal"`
 }
