08eebac3e9401fee6e39334c154492446f6a567e
Ais <madebyais@gmail.com>
2017-04-03 20:09:44 +0700

    Fixed query.

--- modules/investor-check/controller.go
+++ modules/investor-check/controller.go
@@ -42,8 +42,8 @@ func FetchDatatables(ctx *iris.Context) {
 	queryTotalData := "SELECT count(cif.*) as \"totalRows\" FROM cif WHERE cif.\"isVerified\" = false AND cif.\"isActivated\" = true "
 
 	if ctx.URLParam("search") != "" {
-		query += "AND (name ~* '" + ctx.URLParam("search") + "' OR id = " + ctx.URLParam("search") + " OR \"createdAt\"::date = '" + ctx.URLParam("search") + "')"
-		queryTotalData += "AND (name ~* '" + ctx.URLParam("search") + "' OR id = " + ctx.URLParam("search") + " OR \"createdAt\"::date = '" + ctx.URLParam("search") + "')"
+		query += "AND name ~* '" + ctx.URLParam("search") + "' OR id = " + ctx.URLParam("search") + " OR \"createdAt\"::date = '" + ctx.URLParam("search") + "' "
+		queryTotalData += "AND name ~* '" + ctx.URLParam("search") + "' OR id = " + ctx.URLParam("search") + " OR \"createdAt\"::date = '" + ctx.URLParam("search") + "' "
 	}
 
 	query += "group by cif.\"name\", cif.\"phoneNo\", cif.\"idCardNo\", \"bankAccountName\", cif.\"taxCardNo\", "
