71f770a65e24d7187c8e5e9bda5e2aa2cbe3b985
Bakti Pratama <baktiayp@gmail.com>
2017-05-30 15:49:10 +0700

    add groupId

--- modules/emergency-loan/model.go
+++ modules/emergency-loan/model.go
@@ -7,5 +7,6 @@ type emergencyLoanBorrower struct {
 	BranchId     uint64     `gorm:"column:branchId" json:"branchId"`
 	OldLoanId    uint64     `gorm:"column:oldLoanId" json:"oldLoanId"`
 	NewLoanId    uint64     `gorm:"column:newLoanId" json:"newLoanId"`
+	GroupId    uint64     `gorm:"column:groupId" json:"groupId"`
 	Status       bool     `gorm:"column:status" json:"status"`
 }
