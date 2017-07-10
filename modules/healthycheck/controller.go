367d0894bf551de40c579d0e5459cd5ee7bfe967
primayudantra <prima.yudantra@gmail.com>
2017-07-10 12:17:52 +0700

    add checking function

--- modules/healthycheck/controller.go
+++ modules/healthycheck/controller.go
@@ -2,22 +2,35 @@ package healthycheck
 
 import (
 	iris "gopkg.in/kataras/iris.v4"
-	"os/exec"
-	"fmt"
+	"bitbucket.org/go-mis/services"
+
+	"bitbucket.org/go-mis/modules/role"
 )
 
 func Checking (ctx *iris.Context){
-	type Status struct{
-		Postgre string `json:"postgre"`
+	query := `SELECT * FROM "sector" LIMIT 1`
+
+	mRole := role.Role{}
+
+	if e := services.DBCPsql.Raw(query).Scan(&mRole).Error; e != nil {
+		ctx.JSON(iris.StatusOK, iris.Map{
+			"status": "failed",
+			"data":   e,
+		})
+		return
 	}
+	ctx.JSON(iris.StatusOK, iris.Map{
+		"status": "success",
+		"data":   "Database Up",
+	})
 
 	// m := []Status{}
 
-	cmd := exec.Command("pg_ctl","-D","/usr/local/var/postgres","status")
-	fmt.Println(cmd)
+	// cmd := exec.Command("pg_ctl","-D","/usr/local/var/postgres","status")
+	// fmt.Println(cmd)
 
-	err := cmd.Start()
-	fmt.Println(err)
+	// err := cmd.Start()
+	// fmt.Println(err)
 
 	// if err != nil{
 	// 	fmt.Println("idup")
