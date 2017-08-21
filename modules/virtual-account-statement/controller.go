45d81b501079b701ad69468fad92025ed4977323
Bakti Pratama <baktiayp@gmail.com>
2017-08-21 18:34:00 +0700

    hotfix show data statement in VA

--- modules/virtual-account-statement/controller.go
+++ modules/virtual-account-statement/controller.go
@@ -22,6 +22,7 @@ func GetVAStatement(ctx *iris.Context) {
 	query += "FROM virtual_account "
 	query += "INNER JOIN r_virtual_account_statement ON r_virtual_account_statement.\"virtualAccountId\" = virtual_account.id "
 	query += "INNER JOIN virtual_account_statement ON virtual_account_statement.id = r_virtual_account_statement.\"virtualAccountStatementId\" "
+	query += "WHERE virtual_account_statement.\"deletedAt\" IS NULL AND virtual_account.\"deletedAt\" IS NULL"
 
 	vaStatmentSubmissionSchema := []VirtualAccountStatementSubmission{}
 	services.DBCPsql.Raw(query).Scan(&vaStatmentSubmissionSchema)
@@ -30,7 +31,7 @@ func GetVAStatement(ctx *iris.Context) {
 	queryTotalBCA += "FROM virtual_account "
 	queryTotalBCA += "INNER JOIN r_virtual_account_statement ON r_virtual_account_statement.\"virtualAccountId\" = virtual_account.id "
 	queryTotalBCA += "INNER JOIN virtual_account_statement ON virtual_account_statement.id = r_virtual_account_statement.\"virtualAccountStatementId\" "
-	queryTotalBCA += "WHERE virtual_account.\"bankName\" = 'BCA' "
+	queryTotalBCA += "WHERE virtual_account.\"bankName\" = 'BCA' AND virtual_account_statement.\"deletedAt\" IS NULL AND virtual_account.\"deletedAt\" IS NULL"
 
 	totalBCA := TransactionByBank{}
 	services.DBCPsql.Raw(queryTotalBCA).Scan(&totalBCA)
@@ -39,7 +40,7 @@ func GetVAStatement(ctx *iris.Context) {
 	queryTotalBRI += "FROM virtual_account "
 	queryTotalBRI += "INNER JOIN r_virtual_account_statement ON r_virtual_account_statement.\"virtualAccountId\" = virtual_account.id "
 	queryTotalBRI += "INNER JOIN virtual_account_statement ON virtual_account_statement.id = r_virtual_account_statement.\"virtualAccountStatementId\" "
-	queryTotalBRI += "WHERE virtual_account.\"bankName\" = 'BRI' "
+	queryTotalBRI += "WHERE virtual_account.\"bankName\" = 'BRI' AND virtual_account_statement.\"deletedAt\" IS NULL AND virtual_account.\"deletedAt\" IS NULL"
 
 	totalBRI := TransactionByBank{}
 	services.DBCPsql.Raw(queryTotalBRI).Scan(&totalBRI)
