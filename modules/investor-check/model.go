9c90c51462bc54fc334dc22b7a45a5eb76a22389
Didi Yudha Perwira <didiyudha@gmail.com>
2017-10-06 14:54:17 +0700

    Adding data table struct

--- modules/investor-check/model.go
+++ modules/investor-check/model.go
@@ -2,29 +2,57 @@ package investorCheck
 
 import "time"
 
-type InvestorCheck struct {
-	ID                     uint64     `gorm:"primary_key" gorm:"column:id" json:"_id"`
-	Name                   string     `gorm:"column:name" json:"name"`
-	PhoneNo                string     `gorm:"column:phoneNo" json:"phoneNo"`
-	IDCardNo               string     `gorm:"column:idCardNo" json:"idCardNo"`
-	IDCardFilename         string     `gorm:"column:idCardFilename" json:"idCardFilename"`
-	TaxCardNo              string     `gorm:"column:taxCardNo" json:"taxCardNo"`
-	TaxCardFilename        string     `gorm:"column:taxCardFilename" json:"taxCardFilename"`
-	BankAccountName        string     `gorm:"column:bankAccountName" json:"bankAccountName"`
-	IsValidated            *bool      `gorm:"column:isValidated" json:"isValidated"`
-	VirtualAccountBankName string     `gorm:"column:virtualAccountBankName" json:"virtualAccountBankName"`
-	VirtualAccountNumber   string     `gorm:"column:virtualAccountNumber" json:"virtualAccountNumber"`
-	InvestorNo             uint64     `gorm:"column:investorNo" json:"investorNo"`
-	CreatedAt              time.Time  `gorm:"column:createdAt" json:"createdAt"`
-	UpdatedAt              time.Time  `gorm:"column:updatedAt" json:"updatedAt"`
-	DeletedAt              *time.Time `gorm:"column:deletedAt" json:"deletedAt"`
-	IsActivated            *bool      `gorm:"column:isActivated" json:"isActivated"`
-	IsVerified             *bool      `gorm:"column:isVerified" json:"isVerified"`
-	IsDeclined             *bool      `gorm:"column:isDeclined" json:"isDeclined"`
-	Status                 string     `json:"status"`
-}
+type (
+	InvestorCheck struct {
+		ID                     uint64     `gorm:"primary_key" gorm:"column:id" json:"_id"`
+		Name                   string     `gorm:"column:name" json:"name"`
+		PhoneNo                string     `gorm:"column:phoneNo" json:"phoneNo"`
+		IDCardNo               string     `gorm:"column:idCardNo" json:"idCardNo"`
+		IDCardFilename         string     `gorm:"column:idCardFilename" json:"idCardFilename"`
+		TaxCardNo              string     `gorm:"column:taxCardNo" json:"taxCardNo"`
+		TaxCardFilename        string     `gorm:"column:taxCardFilename" json:"taxCardFilename"`
+		BankAccountName        string     `gorm:"column:bankAccountName" json:"bankAccountName"`
+		IsValidated            *bool      `gorm:"column:isValidated" json:"isValidated"`
+		VirtualAccountBankName string     `gorm:"column:virtualAccountBankName" json:"virtualAccountBankName"`
+		VirtualAccountNumber   string     `gorm:"column:virtualAccountNumber" json:"virtualAccountNumber"`
+		InvestorNo             uint64     `gorm:"column:investorNo" json:"investorNo"`
+		CreatedAt              time.Time  `gorm:"column:createdAt" json:"createdAt"`
+		UpdatedAt              time.Time  `gorm:"column:updatedAt" json:"updatedAt"`
+		DeletedAt              *time.Time `gorm:"column:deletedAt" json:"deletedAt"`
+		IsActivated            *bool      `gorm:"column:isActivated" json:"isActivated"`
+		IsVerified             *bool      `gorm:"column:isVerified" json:"isVerified"`
+		IsDeclined             *bool      `gorm:"column:isDeclined" json:"isDeclined"`
+		Status                 string     `json:"status"`
+	}
 
-type InvestorNumber struct {
-	ID         uint64 `gorm:"primary_key" gorm:"column:id" json:"id"`
-	InvestorNo int    `gorm:"column:investorNo" json:"investorNo"`
-}
+	InvestorNumber struct {
+		ID         uint64 `gorm:"primary_key" gorm:"column:id" json:"id"`
+		InvestorNo int    `gorm:"column:investorNo" json:"investorNo"`
+	}
+
+	DataTable struct {
+		Columns   []DataColumn `json:"columns"`
+		OrderInfo `json:"order"`
+		Start     uint64 `json:"start"`
+		Length    uint64 `json:"length"`
+		Search    `json:"search"`
+	}
+
+	DataColumn struct {
+		Data       string `json:"data"`
+		Name       string `json:"name"`
+		Searchable bool   `json:"searchable"`
+		Orderable  bool   `json:"ordrable"`
+		Search     `json:"search"`
+	}
+
+	Search struct {
+		Value string `json:"value"`
+		Regex bool   `json:"regex"`
+	}
+
+	OrderInfo struct {
+		Column uint64 `json:"column"`
+		Dir    string `json:"dir"`
+	}
+)
