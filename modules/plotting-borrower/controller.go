b590be184f8cceeed4af1b86335c1675f590ce82
alfin akhret <alfin.akhret@amartha.com>
2017-11-02 17:41:21 +0700

    fix update investor criteria endpoint

--- modules/plotting-borrower/controller.go
+++ modules/plotting-borrower/controller.go
@@ -3,10 +3,8 @@ package plottingBorrower
 import (
 	"encoding/json"
 	"errors"
-	"strconv"
-	"strings"
 
-	investor "bitbucket.org/go-mis/modules/investor"
+	"bitbucket.org/go-mis/modules/investor"
 	"bitbucket.org/go-mis/services"
 
 	"gopkg.in/kataras/iris.v4"
@@ -26,45 +24,51 @@ func SavePlottingParams(ctx *iris.Context) {
 		return
 	}
 
-	// get investor id
-	var invId uint64
-	s := pp[1 : len(pp)-1]
-
-	ppArr := []string{}
-
-	if strings.Contains(s, ",") {
-		ppArr = strings.Split(s, ",")
-	} else {
-		ppArr = append(ppArr, s)
-	}
-
-	for _, val := range ppArr {
-		if strings.Contains(val, "investorId") {
-			v := strings.Split(val, ":")
-			if strings.Contains(v[1], "\"") || strings.Contains(v[1], "'") {
-				v[1] = v[1][1 : len(v[1])-1]
-			}
-			v[1] = strings.TrimSpace(v[1])
-			v[1] = strings.TrimSuffix(v[1], "\n")
-			v[1] = strings.TrimSuffix(v[1], "\"")
-			id, err := strconv.Atoi(v[1])
-			if err != nil {
-				err := errors.New("Error converting investorId.")
-				ctx.JSON(iris.StatusInternalServerError, iris.Map{
-					"status":  "error",
-					"message": err.Error(),
-				})
-				return
-			}
-			invId = uint64(id)
-			break
-		}
-	}
+	in := []byte(pp)
+	var raw map[string]interface{}
+	json.Unmarshal(in, &raw)
 
 	// set plottingParrams as borrowerCrtiteria on investor
-	inv := &investor.Investor{ID: invId}
-	services.DBCPsql.Model(&inv).Update("borrowerCriteria", pp)
+	var invId uint64
+	val, ok := raw["investorId"].(float64)
+	if !ok {
+		err := errors.New("ERROR: investorId is not a number")
+		ctx.JSON(iris.StatusInternalServerError, iris.Map{
+			"status":  "error",
+			"message": err.Error(),
+		})
+		return
+	}
+	invId = uint64(val)
 
+	var isBorrowerCriteriaActive bool
+	val2, ok := raw["isBorrowerCriteriaActive"].(bool)
+	if !ok {
+		err := errors.New("ERROR: IsBorrowerCriteriaActive is not a bool")
+		ctx.JSON(iris.StatusInternalServerError, iris.Map{
+			"status":  "error",
+			"message": err.Error(),
+		})
+		return
+	}
+	isBorrowerCriteriaActive = val2
+
+	delete(raw, "investorId")
+	delete(raw, "isBorrowerCriteriaActive")
+	out, _ := json.Marshal(raw)
+
+	// model to update
+	inv := investor.Investor{
+		ID: invId,
+	}
+
+	// new data
+	data := map[string]interface{}{
+		"borrowerCriteria":         out,
+		"isBorrowerCriteriaActive": isBorrowerCriteriaActive,
+	}
+
+	services.DBCPsql.Model(&inv).Updates(data)
 	ctx.JSON(iris.StatusOK, iris.Map{
 		"status": "success",
 	})
@@ -150,7 +154,6 @@ func GetPlottingParamsDetail(ctx *iris.Context) {
 	in := []byte(plottingParams.BorrowerCriteriaJSONB)
 	var raw map[string]interface{}
 	json.Unmarshal(in, &raw)
-	raw["count"] = 1
 	plottingParams.BorrowerCriteria = raw
 
 	ctx.JSON(iris.StatusOK, iris.Map{
