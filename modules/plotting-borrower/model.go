111edd5404fd43bc56a6c9a62353d5b4d41b8c39
Bakti Pratama <baktiayp@gmail.com>
2017-11-04 21:01:19 +0700

    filter rate and plafond

--- modules/plotting-borrower/model.go
+++ modules/plotting-borrower/model.go
@@ -6,3 +6,40 @@ type EligbleInvestor struct {
 	InvestorNo       string `gorm:"column:investorNo" json:"investorNo"`
 	BorrowerCriteria string `gorm:"column:borrowerCriteria" json:"borrowerCriteria"`
 }
+
+//TODO move to go-loan
+type BorrowerCriteria struct{
+	Area				[]Area		`json:"area"`
+	Rate				Rate		`json:"rate"`
+	Tenor				[]int		`json:"tenor"`
+	Plafon				Plafon		`json:"plafond"`
+	Sector				[]Sector		`json:"sector"`
+	CreditScoreGrade	[]string	`json:"creditScoreGrade"`
+
+}
+
+//TODO move to go-loan
+type Area struct{
+	ID               int `json:"_id"`
+	Name             string `json:"name"`
+}
+
+//TODO move to go-loan
+type Rate struct{
+	To               float64 `json:"to"`
+	From             float64 `json:"from"`
+	OptionType       uint64  `json:"optionType"`
+}
+
+//TODO move to go-loan
+type Plafon struct{
+	To               int `json:"to"`
+	From             int `json:"from"`
+	OptionType       uint64  `json:"optionType"`
+}
+
+//TODO move to go-loan
+type Sector struct{
+	ID               int `json:"_id"`
+	Name             string `json:"name"`
+}
