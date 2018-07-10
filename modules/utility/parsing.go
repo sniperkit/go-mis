e43d172c49d571977cdea7c0022df00770c0893e
Habib Ridho <habibridho@live.com>
2018-07-10 20:47:17 +0700

    fix type assertion

--- modules/utility/parsing.go
+++ modules/utility/parsing.go
@@ -3,13 +3,8 @@ package utility
 import "strconv"
 
 func ParseBranchIDFromContext(bid interface{}) uint64 {
-	str, ok := bid.(string)
+	branchID, ok := bid.(uint64)
 	if !ok {
-		return 0
-	}
-
-	branchID, err := strconv.ParseUint(str, 10, 64)
-	if err != nil {
 		return 0
 	}
 
